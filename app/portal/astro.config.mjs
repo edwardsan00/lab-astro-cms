5ff5f6d9a80b5602918830c35a36ef47875691b8
edward-sanchez_pacifico <edward.sanchez@softtek.com>
2025-03-24 18:41:00 -0400

    funcional temporal

--- app/portal/astro.config.mjs
+++ app/portal/astro.config.mjs
@@ -1,21 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-
-  // For generating static sites
-  output: "static",
-
+  integrations: [
+    react({
+      experimentalReactChildren: true,
+    }),
+  ],
   vite: {
-    resolve: {
-      alias: {
-        "@ecommerce/ui": path.resolve("../../packages/ui/dist"),
-      },
-    },
+    plugins: [tailwindcss()],
   },
+  output: "static",
 });
